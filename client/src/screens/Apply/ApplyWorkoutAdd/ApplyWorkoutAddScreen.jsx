776da27edec9a255e3bea3815c281f098389abf5
LeoBang <bluke8489@gmail.com>
2021-10-22 16:12:15 +0900

    feat: frams for apply-workout-add-screen

--- client/src/screens/Apply/ApplyWorkoutAdd/ApplyWorkoutAddScreen.jsx
+++ client/src/screens/Apply/ApplyWorkoutAdd/ApplyWorkoutAddScreen.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import axios from 'axios';
+import React, { useState, useEffect} from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { URI } from '../..';
+import { LongBtn } from '../../../components';
 
-const ApplyWorkoutAddScreen = () => {
+const ApplyWorkoutAddScreen = ({ navigation }) => {
+    // State
+    const [categories, setCategories] = useState([]);
+    const [workoutName, setWorkoutName] = useState("");
+    
+    // Hook
+    useEffect(() => {
+        const getCategoryList = async () => {
+            const res = await axios.get(`${URI}/category`)
+            setCategories(res.data);
+        }
+        getCategoryList();
+    })
+
+
     return (
         <View>
-            <Text>apply workout- add 하는 screen</Text>
+            <TextInput 
+                style = {styles.inputTextBox}
+                placeholder = "추가할 운동의 이름을 입력하세요."
+                onChangeText = {(value) => setWorkoutName(value)}
+            />
+            <View>
+                <Text>
+                    카테고리
+                </Text>
+            </View>
+            <View>
+                <Text>
+                    타입
+                </Text>
+            </View>
+            
+            <LongBtn 
+                params = {{innerText: "운동 추가하기", type: "addWorkoutBtn"}}
+                onPress = {() => navigation.goBack()}
+            />
         </View>
     )
 }
 
 export default ApplyWorkoutAddScreen
+
+const styles = StyleSheet.create({
+    inputTextBox: {
+        
+    }
+})
