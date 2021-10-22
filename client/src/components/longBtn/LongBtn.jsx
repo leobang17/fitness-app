f748362e350bf84487ee2a544bf74d13fd0a3abe
LeoBang <bluke8489@gmail.com>
2021-10-22 16:30:15 +0900

    feat: create text input

--- client/src/components/longBtn/LongBtn.jsx
+++ client/src/components/longBtn/LongBtn.jsx
@@ -11,13 +11,18 @@ const LongBtn = ({ params, onPress }) => {
                 styles.container,
                 (params.type === "goToAdd")
                 ? styles.container__goToAdd
+                : (params.type === "addWorkoutBtn")
+                ? styles.container__addWorkoutBtn
                 : null
             ]}
             onPress = {() => onPress()}
         >
             <Text
                 style = {[
-                    styles.btnText
+                    styles.btnText,
+                    (params.type === "addWorkoutBtn")
+                    ? styles.btnText__addWorkoutBtn
+                    : null
                 ]}
             >
                 {params.innerText}
@@ -34,14 +39,18 @@ const styles = StyleSheet.create({
         height: 35,
         borderRadius: 30,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowOffset: { width: 5, height: 5},
+        shadowColor: "gray",
+        shadowOpacity: 0.5
     },
     container__goToAdd: {
         backgroundColor: "#E8E1D9",
         width: width * 0.8,
-        shadowOffset: { width: 5, height: 5},
-        shadowColor: "gray",
-        shadowOpacity: 0.5
+    },
+    container__addWorkoutBtn: {
+        backgroundColor: "#87C5D6",
+        width: width * 0.8,
     },
     btnText: {
         fontSize: 15,
@@ -49,6 +58,10 @@ const styles = StyleSheet.create({
     },
     btnText__goToAdd: {
         
+    },
+    btnText__addWorkoutBtn: {
+        color: "white",
+        fontWeight: "normal"
     }
 
 })
--- client/src/screens/Apply/ApplyWorkoutAdd/ApplyWorkoutAddScreen.jsx
+++ client/src/screens/Apply/ApplyWorkoutAdd/ApplyWorkoutAddScreen.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import React, { useState, useEffect} from 'react'
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Dimensions } from 'react-native'
 import { URI } from '../..';
 import { LongBtn } from '../../../components';
+
+const { width } = Dimensions.get("window");
 
 const ApplyWorkoutAddScreen = ({ navigation }) => {
     // State
@@ -20,21 +22,25 @@ const ApplyWorkoutAddScreen = ({ navigation }) => {
 
 
     return (
-        <View>
-            <TextInput 
-                style = {styles.inputTextBox}
-                placeholder = "추가할 운동의 이름을 입력하세요."
-                onChangeText = {(value) => setWorkoutName(value)}
-            />
+        <View style = {styles.container}>
+            <View style = {styles.inputTextBox__area}>
+                <TextInput 
+                    style = {styles.inputTextBox}
+                    placeholder = "추가할 운동의 이름을 입력하세요."
+                    onChangeText = {(value) => setWorkoutName(value)}
+                />
+            </View>
             <View>
                 <Text>
                     카테고리
                 </Text>
             </View>
             <View>
-                <Text>
-                    타입
-                </Text>
+                <View>
+                    <Text>
+                        타입
+                    </Text>
+                </View>
             </View>
             
             <LongBtn 
@@ -48,7 +54,23 @@ const ApplyWorkoutAddScreen = ({ navigation }) => {
 export default ApplyWorkoutAddScreen
 
 const styles = StyleSheet.create({
+    container: {
+      flexDirection: "column",
+      width: width * 0.85,
+      alignSelf: 'center',
+    },
+    inputTextBox__area: {
+        marginVertical: 10,
+        paddingTop: 10,
+        paddingBottom: 2,
+        borderBottomWidth: 2,
+        borderBottomColor: "#D5D5D5",
+        // borderBottomStartRadius: 10,
+        // borderBottomEndRadius: 10,
+        flexGrow: 1,
+    },
     inputTextBox: {
-        
+        marginLeft: 5
     }
+    
 })
